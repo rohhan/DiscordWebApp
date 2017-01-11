d40020e115d038fa491ec89b141fd6a47944a64d
Rohhan Kochhar <rohhan@gmail.com>
2017-01-11 15:25:32 -0800

    Added data annotations for validations and display on User model.

--- DiscordWebApp/Models/User.cs
+++ DiscordWebApp/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,12 @@ namespace DiscordWebApp.Models
     public class User
     {
         public int Id { get; set; }
+
         public string Username { get; set; }
         public DateTime DateCreated { get; set; }
+
+        [Range(1, 100)]
+        [Display(Name="Number of Flowers")]
         public int NumberOfFlowers { get; set; }
         public int ServerId { get; set; }
     }
