b51be5cde1c43c99151b9765e60652f713b534a9
Rohhan Kochhar <rohhan@gmail.com>
2017-01-10 19:32:08 -0800

    Added a ServerListViewModel so we can project extra data (count of servers) in controller.

--- DiscordWebApp/Controllers/HomeController.cs
+++ DiscordWebApp/Controllers/HomeController.cs
@@ -13,7 +13,26 @@ namespace DiscordWebApp.Controllers
 
         public ActionResult Index()
         {
-            var model = _db.Servers.ToList();
+            //var model =
+            //    from s in _db.Servers
+            //    select new ServerListViewModel
+            //    {
+            //        Id = s.Id,
+            //        Name = s.Name,
+            //        ServerOwner = s.ServerOwner,
+            //        CountOfUsers = s.Users.Count()
+            //    };
+
+            var model =
+                _db.Servers
+                    .Select(s => new ServerListViewModel
+                    {
+                        Id = s.Id,
+                        Name = s.Name,
+                        ServerOwner = s.ServerOwner,
+                        CountOfUsers = s.Users.Count()
+                    }
+                    );
 
             return View(model);
         }
---
+++ DiscordWebApp/Models/ServerListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiscordWebApp.Models
+{
+    public class ServerListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ServerOwner { get; set; }
+        public int CountOfUsers { get; set; }
+    }
+}
