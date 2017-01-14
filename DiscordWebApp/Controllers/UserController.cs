541e3139bd3b29caa2dcf47945fc2da650583c30
Rohhan Kochhar <rohhan@gmail.com>
2017-01-14 01:25:25 -0800

    Added pagination for Users.

--- DiscordWebApp/Controllers/UserController.cs
+++ DiscordWebApp/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace DiscordWebApp.Controllers
 {
@@ -13,11 +14,14 @@ namespace DiscordWebApp.Controllers
         DiscordWebAppDb _db = new DiscordWebAppDb();
 
         // GET: User
-        public ActionResult Index([Bind(Prefix = "id")] int serverId)
+        public ActionResult Index([Bind(Prefix = "id")] int serverId, int page = 1)
         {
             var server = _db.Servers.Find(serverId);
+            ViewBag.ServerName = server.Name;
+            ViewBag.ServerId = server.Id;
+
             if (server != null) {
-                return View(server);
+                return View(server.Users.OrderBy(x => x.Username).ToPagedList(page, 10));
             }
             return HttpNotFound();
         }
